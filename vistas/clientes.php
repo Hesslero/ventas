ae5b6e27fb1c5a1fd792020f88217c4e79d4422f
Hesslero <77806046+Hesslero@users.noreply.github.com>
2023-03-08 14:43:48 -0600

    Initial commit

---
+++ vistas/clientes.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    if(isset($_SESSION['usuario'])){
+    //echo $_SESSION['usuario'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clientes</title>
+    <?php require_once "menu.php"; ?>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+    }else{
+        header("location:../index.php");
+    }
+?>
